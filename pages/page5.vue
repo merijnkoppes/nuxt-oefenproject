2599ce3c7d7a554ffe5afd498501e1f862c9e3aa
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2025-02-06 15:13:47 +0100

    api call

---
+++ pages/page5.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
+<template>
+  <div>
+    <!-- <p>{{ products }}</p> -->
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        <br />
+        <p>{{ product.title }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
