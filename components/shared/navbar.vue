14d2be8a30ab82a930d781044fdf9a704c6b0078
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2025-02-06 15:28:16 +0100

    fetch enkel item

--- components/shared/navbar.vue
+++ components/shared/navbar.vue
@@ -93,6 +93,14 @@
           </li>
           <li>
             <NuxtLink
+              to="/page6"
+              :prefetch="true"
+              class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >Pagina 6</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink
               to="/nestedpage1"
               :prefetch="true"
               class="block py-2 px-3 md:p-0 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
---
+++ pages/page6.vue
@@ -0,0 +1,19 @@
+<script cript setup>
+const id = ref(1);
+const { data: product, refresh } = await useFetch(() => {
+  return `https://fakestoreapi.com/products/${id.value}`;
+});
+const fetchproduct = async () => {
+  ++id.value;
+  await refresh();
+};
+console.log(product.value);
+</script>
+<template>
+  <div>
+    <button @click="fetchproduct"><h1>Display new prroductt</h1></button>
+    <div>
+      {{ product }}
+    </div>
+  </div>
+</template>
