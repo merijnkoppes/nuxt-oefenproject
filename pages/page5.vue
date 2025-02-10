b9ff280d0c481d46d471fdcd6b267d02e1e65317
MerijnKoppes <koppesmerijn@gmail.com>
2025-02-10 14:21:07 +0100

    85 createerror()

--- pages/page5.vue
+++ pages/page5.vue
@@ -1,14 +1,19 @@
 <script setup>
-const { data: products } = await useFetch("https://fakestoreapi.com/products");
+const { data: product, error } = await useFetch(
+  "https://fakestoreapi.com/products/1"
+);
+if (product) {
+  throw createError({ statusCode: 404, message: "page not found" });
+}
 </script>
 <template>
   <div>
     <!-- <p>{{ products }}</p> -->
-    <ul>
-      <li v-for="product in products" :key="product.id">
-        <br />
-        <p>{{ product.title }}</p>
-      </li>
-    </ul>
+    <!-- <ul> -->
+    <!-- <li v-for="product in products" :key="product.id">
+        <br /> -->
+    <p>{{ product.title }}</p>
+    <!-- </li>
+    </ul> -->
   </div>
 </template>
